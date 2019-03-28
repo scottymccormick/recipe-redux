518d35f5e076ecb34243dd67a410701a481b6087
Scott McCormick <scott.t.mccormick@gmail.com>
2019-03-28 15:52:24 -0700

    remove default style, add foundation-sites and sample list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Menu, MenuItem, Alignments } from 'react-foundation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Recipes with Redux</h1>
         </header>
+        <main>
+          <h3>Ingredients</h3>
+          <Menu isVertical>
+            <MenuItem>Item Text</MenuItem>
+            <MenuItem>Item Text</MenuItem>
+            <MenuItem>Item Text</MenuItem>
+          </Menu>
+        </main>
       </div>
     );
   }
