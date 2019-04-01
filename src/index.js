441138dcfed1098b351bb2afb9029ec596821c34
Scott McCormick <scott.t.mccormick@gmail.com>
2019-04-01 14:39:06 -0700

    include redux devtools in create store.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
